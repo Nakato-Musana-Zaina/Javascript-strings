b01d0dd1085646e26fbd31c23e704c32026e5ed7
Nakato-Musana-Zaina <nakatozainamusana@gmail.com>
2024-03-13 14:42:25 +0300

    Added new strings

--- strings.js
+++ strings.js
@@ -13,14 +13,16 @@ console.log(word[12]);
 // Insert the following string at the fourth index of the below variable:"eat"const food = "The quick fox jumped over the lazy dog"
 
 const food = 'The quick Fox jumped over the lazy dog';
-const string = food.slice(0,4) + " " + "eat" + food.slice(4);
-console.log({string});
+const food1 = 'The '+'eat'+'quick fox jumped over the lazy dog';
+console.log({food1});
 
 
 // Count how many times the following string appears in the string variable:1. "the"2."brown"const story= "The quick brown fox jumps over the lazy dog"
 const story = 'The quick brown fox jumps over the lazy dog';
 console.log((story.match(/the/g)).length);
 console.log((story.match(/brown/g)).length);
+const storys = story.split(story).length -1;
+console.log(storys)
 
 
 // Using JavaScript, find the following words from the following strings:1. "are"const string1 = "The pupils are reading in the library";2. "sitting"const string2 = "The child was sitting on the table before it fell"
@@ -46,6 +48,25 @@ console.log({index: string2.indexOf('sitting')});
 //  let titlecasecomment =comment.title();
 //  console.log({titlecasecomment});
 
+let a = 'fine books of literature';
+let length = a.length;
+console.log(a.length);
+ let b = 'Elephants';
+ let blength = b.length;
+ console.log({length: b.length})
+
+ let c ='tiny monkeys';
+ let at  = c.at(0,3);
+ console.log({extract: c.at(0)});
+
+ let d = c.charAt(5);
+ console.log({extracted: c.charAt(5)})
+
+ const e = 'Chicken wings';
+ console.log({index: e.at(3)});
+ console.log({index: e[3] });
+
+ let text = 'mango, pineappleapples'
 
 
 
